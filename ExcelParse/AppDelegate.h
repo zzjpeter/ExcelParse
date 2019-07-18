43745dbfcdf3a289224fafe53c9e102a4eb8d1d4
zhuzhijia <zhuzhijia@lespark.us>
2019-07-18 10:11:30 +0800

    Initial Commit

---
+++ ExcelParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExcelParse
+//
+//  Created by 朱志佳 on 2019/7/18.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExcelParse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExcelParse
+//
+//  Created by 朱志佳 on 2019/7/18.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
